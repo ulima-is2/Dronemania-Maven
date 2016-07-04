d8cdb67920285ad66aae90608d30d525caafdc96
PROFW52 <PROFW52@lab.aca>
2016-07-04 18:09:54 -0500

    Primer push

--- src/main/java/pe/edu/ulima/controllers/LoginServlet.java
+++ src/main/java/pe/edu/ulima/controllers/LoginServlet.java
@@ -1,6 +1,6 @@
 package pe.edu.ulima.controllers;
 
-import com.google.gson.Gson;
+import com.google.gson.Gson; 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
